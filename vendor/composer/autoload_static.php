875632d6fca3310968f4e40e937cbc7a5331d619
woodab <101159550+woodab@users.noreply.github.com>
2024-01-06 09:57:22 -0700

    a

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0229f86023279debae8e7bbc33335dc3
 {
+    public static $prefixLengthsPsr4 = array (
+        'Y' => 
+        array (
+            'YourNamespace\\' => 14,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'YourNamespace\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +27,8 @@ class ComposerStaticInit0229f86023279debae8e7bbc33335dc3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0229f86023279debae8e7bbc33335dc3::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0229f86023279debae8e7bbc33335dc3::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit0229f86023279debae8e7bbc33335dc3::$classMap;
 
         }, null, ClassLoader::class);
