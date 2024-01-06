e078282336b5e9c844bb9d2738e51cb9c2b62073
woodab <101159550+woodab@users.noreply.github.com>
2024-01-06 09:41:25 -0700

    a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor-name/project-name',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dc740491ff1513126294733044fd1311a1c47684',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor-name/project-name' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dc740491ff1513126294733044fd1311a1c47684',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
