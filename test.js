6ccd6634405d6df98f98fa7d761f03152cb7675f
woodab <101159550+woodab@users.noreply.github.com>
2024-01-06 07:56:08 -0700

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-function unsafeFunction(userInput) {
-  // A risky use of eval, executing user-provided input
-  eval(userInput);
-}
-
-let userProvidedData = "console.log('Executing user code!')";
-unsafeFunction(userProvidedData);
